4dd38298df8d6a94ea042576054e8ac4eb58c472
Aviv Naaman <avivnaaman04@gmail.com>
2022-11-16 12:59:33 +0200

    More examples - AppCfg + X methods

---
+++ ExtensionMethods/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ExtensionMethods
+{
+	public class Person
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+}
+
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ExtensionMethods;
+
+// must be static!
+public static class PersonExtensions
+{
+    public static string GetFullName(this Person person)
+    {
+        return $"{person.FirstName} {person.LastName}";
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var p = new Person() { FirstName = "Dor", LastName = "Cohen" };
+        var full_name = p.GetFullName();
+        Console.WriteLine(full_name);
+    }
+}
