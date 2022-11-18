197ffc2e6cd830d73db2c305f4a9a86139c535fe
Aviv Naaman <avivnaaman04@gmail.com>
2022-11-18 14:28:31 +0200

    Add part of app.config example including basic parsing

--- AppCfgExample/Program.cs
+++ AppCfgExample/Program.cs
@@ -1,3 +1,28 @@
 ﻿using System.Configuration;
 
-Console.WriteLine("Hello, World!");
+namespace AppCfgExmaple;
+
+public class BehaviorSettings : ConfigurationSection
+{
+    [ConfigurationProperty(name: "refreshTimeout", IsRequired = false, DefaultValue = 50)]
+    public int RefreshTimeout { get { return (int)this["refreshTimeout"]; } }
+}
+
+
+public class Program {
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // Using appSettings
+        var secretPasswordValue = ConfigurationManager.AppSettings["SecretPwd"];
+        Console.WriteLine($"Secret Password is: '{secretPasswordValue}'");
+
+        // ConnectionStrings are similar and relevant to Databases
+        var connectionString = ConfigurationManager.ConnectionStrings["Main"];
+        Console.WriteLine($"Connection Strings are somewhat similar: '{connectionString}'");
+
+        var myCustomCfg = (BehaviorSettings)System.Configuration.ConfigurationManager.GetSection("Behavior");
+        Console.WriteLine($"Current Refresh Timeout is: {myCustomCfg.RefreshTimeout}");
+    }
+}
